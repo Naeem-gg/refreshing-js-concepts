2589915a96d0346f455382cde5649074e3b419c8
Naeemgg <naeemgg@duck.com>
2022-09-05 16:08:55 +0530

    leap year and love calculator added

---
+++ leapYear.js
@@ -0,0 +1,27 @@
+function isLeap(year) 
+{
+    if(year % 4 === 0)
+    {
+        if(year % 100 === 0)
+        {
+            if(year % 400 === 0)
+            {
+                console.log("leap year.");
+            }
+            else
+            {
+                console.log("Not leap year");
+            }
+        }
+        else
+        {
+            console.log("leap year.");
+        }
+    }
+    else
+    {
+        console.log("Not leap year.");
+    }
+}
+
+isLeap(2004);
---
+++ loveCal.js
@@ -0,0 +1,6 @@
+let first = prompt("Enter your name Sabbo bachhi");
+let second = prompt("Enter unka nam Saboo bachhi");
+
+let loveCal = Math.random() * 100;
+
+alert(`Love between ${first} and ${second} is ${Math.floor(loveCal)+1}% `);
