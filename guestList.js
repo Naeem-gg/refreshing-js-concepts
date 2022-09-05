57b434d1779e72f93a98eb7518325cbfde8d7557
Naeemgg <naeemgg@duck.com>
2022-09-05 16:42:39 +0530

    FizzBuzz

---
+++ guestList.js
@@ -0,0 +1,12 @@
+let guestList = ["shahbaz", "naeem", "faisal", "muzammil", "maaz", "aamir"];
+let myname = "naeem";
+
+if(guestList.includes(myname))
+{
+console.log("welcome "+ myname);
+}
+else
+{
+    console.log("who are you?")
+}
+// console.log(guestList);
